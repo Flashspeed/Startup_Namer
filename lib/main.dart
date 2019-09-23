bb0ffb7ea3be545c3c26dcd6e703903eb955e702
Ife <ife.amusan@matrixtelematics.com>
2019-09-23 10:59:18 +0100

    Use an external package

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
 // https://flutter.dev/docs/get-started/codelab
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    String generatedName;
+
+    generateWordPairs().take(1).forEach( (word) => generatedName = word.asPascalCase );
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,7 +23,10 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Text('Hello World', textScaleFactor: 2),
+              Text(
+                  generatedName,
+                  textScaleFactor: 2
+              ),
               RaisedButton(
                 child: Text("Press"),
                 onPressed: () {},
